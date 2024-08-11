30e9c1d3209b3aaf355f3e2894fe2f57228ae607
Ratna Katuwal <115388990+Ratna-katuwal1@users.noreply.github.com>
2024-08-11 21:32:25 +0545

    initial commit

--- app/src/main/java/com/ratna/harry_potter/adapter/CharacterAdapter.java
+++ app/src/main/java/com/ratna/harry_potter/adapter/CharacterAdapter.java
@@ -1,4 +1,92 @@
 package com.ratna.harry_potter.adapter;
 
-public class CharacterAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.ratna.harry_potter.CharacterDetailActivity;
+import com.ratna.harry_potter.R;
+import com.ratna.harry_potter.models.HarryPotterCharacter;
+
+import java.util.ArrayList;
+
+public class CharacterAdapter extends RecyclerView.Adapter<CharacterAdapter.CharacterViewHolder> {
+    private Context context;
+    private ArrayList<HarryPotterCharacter> characterList;
+
+    public CharacterAdapter(Context context, ArrayList<HarryPotterCharacter> characterList) {
+        this.context = context;
+        this.characterList = characterList;
+    }
+
+    @NonNull
+    @Override
+    public CharacterAdapter.CharacterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_character, parent, false);
+        return new CharacterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CharacterAdapter.CharacterViewHolder holder, int position) {
+HarryPotterCharacter character = characterList.get(position);
+
+holder.characterName.setText(character.getName());
+holder.actorName.setText("Played by: " + character.getActor());
+holder.dateOfBirth.setText("DOB: " + character.getDateOfBirth());
+holder.gender.setText("Gender: " +character.getGender());
+holder.house.setText("House: " + character.getHouse());
+
+StringBuilder akaBuilder = new StringBuilder("A.K.A: ");
+for (String name : character.getAlternate_names()) {
+    akaBuilder.append(name).append(", ");
+}
+if (akaBuilder.length() > 0) {
+    akaBuilder.setLength(akaBuilder.length() - 2);
+}
+holder.alternateNames.setText(akaBuilder.toString());
+
+Glide.with(context)
+.load(character.getImage())
+.into(holder.characterImage);
+
+holder.itemView.setOnClickListener(view -> {
+    Intent intent = new Intent(context, CharacterDetailActivity.class);
+    intent.putExtra("character", character);
+    context.startActivity(intent);
+});
+    }
+
+    @Override
+    public int getItemCount() {
+        return characterList.size();
+    }
+
+    public class CharacterViewHolder extends RecyclerView.ViewHolder {
+        ImageView characterImage;
+        TextView characterName;
+        TextView actorName;
+        TextView dateOfBirth;
+        TextView gender;
+        TextView house;
+        TextView alternateNames;
+
+        public CharacterViewHolder(View itemView) {
+            super(itemView);
+            characterImage = itemView.findViewById(R.id.character_image);
+            characterName = itemView.findViewById(R.id.character_name);
+            actorName = itemView.findViewById(R.id.actor_name);
+            dateOfBirth = itemView.findViewById(R.id.date_of_birth);
+            gender = itemView.findViewById(R.id.gender);
+            house = itemView.findViewById(R.id.house);
+            alternateNames = itemView.findViewById(R.id.alternate_names);
+        }
+    }
 }
--- app/src/main/java/com/ratna/harry_potter/adapter/QuizAdapter.java
+++ app/src/main/java/com/ratna/harry_potter/adapter/QuizAdapter.java
@@ -1,4 +1,115 @@
 package com.ratna.harry_potter.adapter;
 
-public class QuizAdapter {
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ratna.harry_potter.R;
+import com.ratna.harry_potter.datas.HarryPotterQuiz;
+
+import java.util.List;
+
+public class QuizAdapter extends RecyclerView.Adapter<QuizAdapter.QuizViewHolder> {
+    private final List<HarryPotterQuiz> quizList;
+    private final Context context;
+
+    public QuizAdapter(Context context, List<HarryPotterQuiz> quizList) {
+        this.context = context;
+        this.quizList = quizList;
+    }
+
+    @NonNull
+    @Override
+    public QuizViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_quiz, parent, false);
+        return new QuizViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull QuizViewHolder holder, int position) {
+HarryPotterQuiz quiz = quizList.get(position);
+int questionNumber = position + 1;
+holder.questionText.setText(questionNumber+". "+ quiz.getQuestion());
+
+        List<String> choices = quiz.getChoices();
+        holder.option1.setText(choices.get(0));
+        holder.option2.setText(choices.get(1));
+        holder.option3.setText(choices.get(2));
+        holder.option4.setText(choices.get(3));
+
+        holder.option1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                handleAnswer(holder,0,quiz.getCorrectAnswer(), choices);
+            }
+        });
+
+        holder.option2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleAnswer(holder,1,quiz.getCorrectAnswer(),choices);
+            }
+        });
+
+        holder.option3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleAnswer(holder,2,quiz.getCorrectAnswer(),choices);
+            }
+        });
+
+
+        holder.option4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleAnswer(holder,3,quiz.getCorrectAnswer(),choices);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return quizList.size();
+    }
+
+    public void handleAnswer(QuizViewHolder holder, int selectedIndex, int correctAnswerPosition, List<String> choice){
+        if (selectedIndex == correctAnswerPosition){
+            holder.feedbackText.setText("Correct!");
+            holder.feedbackText.setTextColor(Color.GREEN);
+        } else {
+            holder.feedbackText.setText("Wrong. Correct answer is: " + choice.get(correctAnswerPosition));
+            holder.feedbackText.setTextColor(Color.RED);
+        }
+        holder.feedbackText.setVisibility(View.VISIBLE);
+    }
+
+    public static class QuizViewHolder extends RecyclerView.ViewHolder {
+        TextView questionText;
+        RadioGroup radioGroup;
+        RadioButton option1;
+        RadioButton option2;
+        RadioButton option3;
+        RadioButton option4;
+        TextView feedbackText;
+
+        public QuizViewHolder(View itemView) {
+            super(itemView);
+            questionText = itemView.findViewById(R.id.question_text);
+            radioGroup = itemView.findViewById(R.id.radio_group);
+            option1 = itemView.findViewById(R.id.option1);
+            option2 = itemView.findViewById(R.id.option2);
+            option3 = itemView.findViewById(R.id.option3);
+            option4 = itemView.findViewById(R.id.option4);
+            feedbackText = itemView.findViewById(R.id.feedback_text);
+        }
+    }
 }
